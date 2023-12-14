1f5a774fcdf892679d803fe813ac8591d2e7b4a2
Abhinav <96286033+AbhinavTheNoobCoder@users.noreply.github.com>
2023-12-14 20:19:46 +0530

    Create Day5.py
    In this code, I learned how to use comments (single-line and multi-line), escape sequences, and the parameters associated with the print() statement in Python.

---
+++ Day5.py
@@ -0,0 +1,13 @@
+#DAY5 OF PYTHON - Learning about comments, print parameters and escape characters
+'''This
+is
+a
+multiline comment'''
+"""So 
+is
+this
+"""
+print("Hi, I am Abhinav.") #end = \n (new line) by default, sep = " " by default
+print("I am a \"Hybrid Panda\"", end = " ") #" " is appended to this if there is another print next
+print("and I am learning Python", "NoobieAbhinav", sep = " ~ ") #the 2 objects are separared by "~"
+#statement 11 - \"Hybrid Panda\" allows me to use "" characters when I am not allowed to do so
