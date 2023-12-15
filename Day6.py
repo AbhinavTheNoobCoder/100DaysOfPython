b588a8e6f6df24057a33fc8db3919e2e809876fa
Abhinav <96286033+AbhinavTheNoobCoder@users.noreply.github.com>
2023-12-15 20:26:03 +0530

    Create Day6.py
    In this code, I learned about variables, how to define them and the different data types associated with a variable. I know about lists, tuples, and dictionaries too, they will feature in future codes.

---
+++ Day6.py
@@ -0,0 +1,9 @@
+#DAY 6 of Python -  Variables and data types
+a = 18 #integer <class 'int'>
+b = "Abhinav" #string <class 'str'>
+c = 1.5 #float
+d = complex(1,2) #complex number
+e = 1 + 2j #direct way of naming complex numbers
+print(a, b, c, d, e, sep= '\n')
+print(type(e)) #type() prints type of data of the object
+#a, b, c, d, e are all variables
