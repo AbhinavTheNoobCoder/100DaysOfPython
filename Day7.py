36695ab03f5e26b7112cce2035eeeb6116ccf7b9
Abhinav <96286033+AbhinavTheNoobCoder@users.noreply.github.com>
2023-12-16 21:06:10 +0530

    Create Day7.py
    For my first exercise in this course, I wrote a code to create a basic calculator that can perform some fundamental arithmetic operations between two given numbers (they are not input by the user) in Python. The output of the code is displayed in a format that is easy to read and understand.

---
+++ Day7.py
@@ -0,0 +1,11 @@
+'''DAY 7 - Using arithmetic operators to create calculator which can calculate basic operations b/w
+two numbers (not input) and display result in human readable format'''
+a = 5
+b = 11
+print(a, "+", b, "=", a+b)
+print(a, "-", b, "=", a-b)
+print(a, "x", b, "=", a*b)
+print(a, "divided by", b, "=", a/b)
+print(a, "to the power of", b, "=", a**b)
+print("Quotient when", a, "is divided by", b, "=", a//b)
+print("Remainder when", a, "is divided by", b, "=", a%b)
