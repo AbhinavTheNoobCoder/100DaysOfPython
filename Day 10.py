adf0dabd377807c3b78b957af19084c8b60dca62
Abhinav <96286033+AbhinavTheNoobCoder@users.noreply.github.com>
2023-12-19 17:52:11 +0530

    Create Day 10.py

---
+++ Day 10.py
@@ -0,0 +1,7 @@
+#DAY 10 - Taking user input
+a = input("Enter your name here: ")
+print("Your name is", a)
+x = input("Enter number 1: ")
+y = input("Enter number 2: ")
+print(x + y) #Will return the wrong answer due to concatenation (input is taken as str by default)
+print(int(x) + int(y)) #gives correct answer unless floating point number is given
