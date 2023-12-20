23841fb3c063b087f0f700ae0c06ca2d9081d624
Abhinav <96286033+AbhinavTheNoobCoder@users.noreply.github.com>
2023-12-20 19:50:09 +0530

    Create Day11.py
    In this code, I got a basic introduction to strings. I also learned how to create multiline strings and how to access string elements through indexing. Also learned how to use the 'for' loop to access string elements in one loop.

---
+++ Day­11.py
@@ -0,0 +1,9 @@
+#DAY11 - Introduction to strings
+name = "Abhinav" #'Abhinav' is also valid
+multi = '''Hello,
+how are you?
+I am under the water,
+here too much raining, uhhh''' #multline string
+print(name[0])
+for i in name:
+    print (i)
