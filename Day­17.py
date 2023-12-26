71cfc6bf8336d967a025b5e5d374e925ed2ad5d1
Abhinav <96286033+AbhinavTheNoobCoder@users.noreply.github.com>
2023-12-26 21:07:28 +0530

    Add files via upload
    In this code, I used a for loop to iterate through a string and through a range of numbers.

---
+++ Day­17.py
@@ -0,0 +1,9 @@
+#DAY 17 - for loops
+for i in "Abhinav":
+    print(i, end = ", ")
+print()
+for num in range(5): #start value is 0 by default, step value is 1
+    print(num, end = ", ")
+print()
+for num in range(2, 9, 3):
+    print(num, end = ", ")
